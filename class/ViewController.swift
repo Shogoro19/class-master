ffd7265421773a786e917d9d37b849fbc756a7d9
藤田祥五朗 <shogoro.56@ezweb.ne.jp>
2018-11-24 17:52:49 +0900

    Initial Commit

---
+++ class/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  class
+//
+//  Created by 上田　護 on 2018/11/23.
+//  Copyright © 2018 mamoru.ueda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let mentor = mentor()//インスタンスを立てて！
+    let enemy:Int = 3;//敵の数
+
+    @IBOutlet var namelabel: UILabel! //何か足りないかも？
+    @IBOutlet var HPlabel: UILabel! //何か足りないかも？
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        namelabel = mentor.name//名前ラベルに反映できるようにしよう！
+        HPlabel = String(mentor.HP)//ラベルに反映できるようにしよう！
+    }
+
+    @IBAction func battleButton(){
+        mentor.battle()//敵の数を引数にしよう！
+    }
+
+}
